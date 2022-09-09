5fa2c249c305c5883da496034b01edc433bbac36
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-09-09 11:46:41 +0500

    learn about git diff

--- python_developer.py
+++ python_developer.py
@@ -1,1 +1,4 @@
-print('learning git')
+print('learning git...')
+
+print("let's see git diff b/w working area and staging directory")
+print("And git diff --staged/Head for working area and local repo")
