c65edba5265f60dcbffe8d25cbff322a531d823a
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-09-09 12:12:36 +0500

    added comments

--- python_developer.py
+++ python_developer.py
@@ -2,3 +2,10 @@ print('learning git...')
 
 print("let's see git diff b/w working area and staging directory")
 print("And git diff --staged/Head for working area and local repo")
+
+# git reverting to earlier commits
+# git log : history of commits associated with each file
+# git checkout : stage the file
+# git status : share the command to unstage file
+# git reset : remove the file from the stagging area & bring it back to working directoty
+# git
