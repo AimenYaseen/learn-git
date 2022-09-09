6af8da8d7856be3f5b3f3bee70b88f5fe7925cd5
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-09-09 15:49:37 +0500

    Lesson#01 by simplilearn

---
+++ getting_started_with_git.py
@@ -0,0 +1,21 @@
+# git diff
+# git diff // Difference b/w working area and staging area
+# git diff --staged/Head // Difference b/w staging area and local repo
+
+# git reverting to earlier commits
+# git log : history of commits associated with each file // git log fileA.py
+# git checkout : stage the file // from local to stage Area
+# git status : share the command to unstage file 
+# git reset : remove the file from the stagging area & bring it back to working directoty
+# cat fileA.py will print the file contents
+# git checkout Head~1 fileA.py
+# git checkout commit_id filename
+# git checkout version/commit_id filename
+
+# Deleting Files
+# git rm filename // remove the file from working + staging area
+# git rm --cached filename // remove the file from staging area
+
+# git ls-files --stage // return all the staged files
+# rm filename // remove the file from working branch
+
